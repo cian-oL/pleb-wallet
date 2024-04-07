3813073f0ac4a180365f135d10cfc2611c9b6162
mecastor <mecastor87@gmail.com>
2024-04-07 18:21:53 +0100

    Added btc price retrieval

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [price, setPrice] = useState(null);
+
+  const getPrice = () => {
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      .then((response) => {
+        setPrice(response.data.data.amount);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getPrice();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="bg-slate-800 font-mono text-purple-500">
       <header className="border-solid border-2 border-slate-800 border-b-yellow-500 text-center">
@@ -11,7 +32,7 @@ function App() {
         </div>
         <div className="bg-yellow-500 border-solid border-2 border-purple-500 rounded p-2 w-1/4 mt-4 mb-2 mr-2 md:w-full md:text-center md:mx-[2%] sm:w-4/5 sm:my-0 sm:mx-auto sm:mt-[1%]">
           <h2>Price</h2>
-          <p className="text-xl font-bold">price</p>
+          <p className="text-xl font-bold">${price}</p>
         </div>
       </div>
       <div className="w-full flex justify-between sm:flex-col">
